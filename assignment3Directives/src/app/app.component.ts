c372301ceeab850d4c2a7fa390b04df1fc02b246
schotti1001 <schottenhammer.markus@posteo.de>
2024-02-17 17:33:54 +0100

    directives, assignment 2 & 3

---
+++ assignment3Directives/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    showSecret = false;
+    allClicks = [];
+
+    toggleVisibility(){
+        this.showSecret = !this.showSecret;
+        var newEntry = this.allClicks.length+1;
+        this.allClicks.push(newEntry);
+        console.log(newEntry)
+    }
+
+    after5(log){
+        return log>5 ? true : false;
+    }
+}
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,8 +2,33 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-servers',
+    // Alternative to templateURl is to use template
+    // template: `
+    //     <app-server></app-server>
+    //     <app-server></app-server>
+    // `
     templateUrl: './servers.component.html'
 })
 export class ServersComponent {
+    allowNewServer = false;
+    serverCreationStatus = 'No server was created!';
+    serverName = '';
+    serverCreated = false;
+    servers = ['Testserver', 'Testserver 2'];
 
+    constructor() {
+        setTimeout(() => {
+            this.allowNewServer = true;
+        }, 2000);
+    }
+
+    onCreateServer() {
+        this.serverCreated = true;
+        this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+        this.servers.push(this.serverName);
+    }
+
+    onUpdateServerName(event: Event) {
+        this.serverName = (<HTMLInputElement>event.target).value;
+    }
 }
