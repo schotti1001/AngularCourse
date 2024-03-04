f4e7c8376a34886eb13b46239faf9a03810af376
schotti1001 <schottenhammer.markus@posteo.de>
2024-03-04 19:45:52 +0100

    example own observable

--- exampleObservable/src/app/home/home.component.ts
+++ exampleObservable/src/app/home/home.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval} from 'rxjs';
+import { Observable } from 'rxjs-compat';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    private firstObsSubscription: Subscription;
 
-  ngOnInit() {
-  }
+    ngOnDestroy(): void {
+        this.firstObsSubscription.unsubscribe();
+    }
+
+    ngOnInit() {
+        // this.firstObsSubscription = interval(1000).subscribe(count => {
+        //     console.log(count);
+        // });
+
+        const customIntervalObservalbe = Observable.create(observer => {
+            let count = 0;
+            setInterval(() => {
+                observer.next(count);
+                if(count === 2) {
+                    observer.complete();
+                }
+                if(count > 3 ) {
+                    observer.error(new Error('Count is greater than 3'));
+                }
+                count ++;
+            }, 1000)
+        });
+
+        this.firstObsSubscription = customIntervalObservalbe.subscribe( data => {
+            console.log(data);
+        }, error => {
+            alert(error.message);
+        }, ( ) => {
+            console.log('Completed!');
+        })
+    }
 
 }
