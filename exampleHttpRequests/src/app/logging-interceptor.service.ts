e539c9a6478379355b3e18d47eb1a3f50087f751
schotti1001 <schottenhammer.markus@posteo.de>
2024-03-06 20:15:50 +0100

    finished example http requests

---
+++ exampleHttpRequests/src/app/logging-interceptor.service.ts
@@ -0,0 +1,16 @@
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+
+export class LoggingInterceptorService implements HttpInterceptor{
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log('Outgoing request');
+        console.log(req.url);
+        return next.handle(req).pipe(tap(event => {
+            if(event.type === HttpEventType.ResponseHeader){
+                console.log('Response arrived');
+                console.log(event);
+            }
+        }));
+    }
+}
--- exampleHttpRequests/src/app/post.service.ts
+++ exampleHttpRequests/src/app/post.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
-import { catchError, map } from "rxjs/operators";
+import { catchError, map, tap } from "rxjs/operators";
 import { Subject, throwError } from "rxjs";
 
 @Injectable({
@@ -14,8 +14,11 @@ export class PostService{
 
     createAndStorePost(title: string, content: string){
         const postData: Post = {title: title, content: content};
-        this.http.post<{name: string}>('https://angular-course-f7b9e-default-rtdb.firebaseio.com/posts.json'
-        , postData).subscribe(responseData => {
+        this.http.post<{name: string}>(
+            'https://angular-course-f7b9e-default-rtdb.firebaseio.com/posts.json'
+        , postData,
+        { observe: 'response'}
+        ).subscribe(responseData => {
             console.log(responseData);
         }, error => {
             this.error.next(error);
@@ -23,7 +26,14 @@ export class PostService{
     }
 
     fetchPosts(){
-       return this.http.get<{[key:string]: Post}>('https://angular-course-f7b9e-default-rtdb.firebaseio.com/posts.json')
+        let searchParams = new HttpParams();
+        searchParams = searchParams.append('print', 'pretty'); //required in case of multiple params
+
+       return this.http.get<{[key:string]: Post}>('https://angular-course-f7b9e-default-rtdb.firebaseio.com/posts.json',
+        {
+            headers: new HttpHeaders({"Custom-Header": 'Hello'}),
+            params: searchParams
+        })
         .pipe(map((responseData) => {
             const postArray: Post[] = [];
             for(const key in responseData) {
@@ -38,6 +48,14 @@ export class PostService{
     }
 
     deleteAll(){
-        return this.http.delete('https://angular-course-f7b9e-default-rtdb.firebaseio.com/posts.json');
+        return this.http.delete('https://angular-course-f7b9e-default-rtdb.firebaseio.com/posts.json', {
+            observe: 'events',
+            responseType: 'json'
+        }).pipe(tap(event => {
+            console.log(event);
+            if(event.type === HttpEventType.Response) {
+                console.log(event.body)
+            }
+        }));
     }
 }
