c27ac10aae2b1061143486baa2dd10f1b0dfd44e
schotti1001 <schottenhammer.markus@posteo.de>
2024-03-20 14:33:05 +0100

    changed to signales

--- angularSignals/src/app/signals/signals.component.ts
+++ angularSignals/src/app/signals/signals.component.ts
@@ -1,5 +1,5 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signals',
@@ -8,16 +8,21 @@ import { Component } from '@angular/core';
   imports: [NgFor],
 })
 export class SignalsComponent {
-  actions: string[] = [];
-  counter = 0;
+  actions = signal<string[]>([]);
+  counter = signal(0);
+  doubleCounter = computed(() => this.counter() * 2);
+
+  constructor(){
+    effect(() => console.log(this.counter()));
+  }
 
   increment() {
-    this.counter++;
-    this.actions.push('INCREMENT');
+    this.counter.update(oldValue => oldValue + 1);
+    this.actions.update(oldV => [...oldV, 'INCREMENT']);
   }
 
   decrement() {
-    this.counter--;
-    this.actions.push('DECREMENT');
+    this.counter.update(oldValue => oldValue - 1);
+    this.actions.update(oldV => [...oldV, 'DECREMENT']);
   }
 }
